1d41f779eb0fb1aa988bfd8577e9ddd67d5dc47b
monishankerw <monishanker121@gmail.com>
2024-10-22 22:12:26 +0530

    start

--- src/main/java/org/example/corejava/leetscode/LeetsCode.java
+++ src/main/java/org/example/corejava/leetscode/LeetsCode.java
@@ -1477,6 +1477,36 @@ return new String(ch);
             return isPalindrome;
         }
     }
+    public static class FirstPalindromeString{
+        public static void main(String[] args) {
+            String[] words={"abc","car","ada","racecar","cool"};
+            String output=firstPalindromeString(words);
+            System.out.println("Output:"+output);
+        }
+
+        private static String firstPalindromeString(String[] words) {
+            for (String word:words){
+                if(isPalindrome(word)){
+                    return word;
+
+                }
+            }
+            return "";
+        }
+
+        private static boolean isPalindrome(String word) {
+            int left=0;
+            int right=word.length()-1;
+            while (left<right){
+                if(word.charAt(left)!=word.charAt(right)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
 
 }
 
